b7041294f74bd216c3274fb68e9918af817789d7
brahma1085 <cctobrahma@gmail.com>
2026-08-18 19:03:31 +0530

    Fix search-service dependencies and enable Feign clients

--- backend/search-service/src/main/java/com/zomato/search_service/SearchServiceApplication.java
+++ backend/search-service/src/main/java/com/zomato/search_service/SearchServiceApplication.java
@@ -3,7 +3,10 @@ package com.zomato.search_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @SpringBootApplication
+@EnableFeignClients
 public class SearchServiceApplication {
 
 	public static void main(String[] args) {
